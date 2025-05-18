086bfac25b74c10c6f29d2969dbe13cb284a373f
wings <limmvzbusines@gmail.com>
2025-05-18 19:18:06 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Fungsi untuk menambahkan produk ke keranjang
+function addToCart(name, price) {
+    alert(`${name} berhasil ditambahkan ke keranjang!`);
+    // Anda bisa mengembangkan ini untuk menyimpan data ke localStorage atau mengirim ke backend
+}
